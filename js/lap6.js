c25b689109e5a0095171ceef2ea0e9a8aa3b8a27
hanan-almukdad <nanamuldad@gmail.com>
2020-02-05 20:26:45 +0200

    add somthing

--- js/lap6.js
+++ js/lap6.js
@@ -12,13 +12,23 @@ return result;
  document.write('<h3>'+ fun() +'</h3>');
  function good (){
      var person = prompt("are you studing here? ");
-     var result;
-
+     while(person.toLocaleLowerCase() !=='yes' && person !=='no'){
+     var person = prompt("are you studing here? ");
+    
+     }
+     var okay =prompt("are you studing here?");
+     console.log(okay);
+     var result="";
+     for(var i=0; i<okay; i++)
+     if(person === 'yes')
+     { result = result + '<img src="img/youjourney-small.png">'; 
+    }   else if (person === 'no')
+    { result = result + '<img src="img/maxerdefault.jpg">';
+    }
 if (person !=null) { 
   result='so '  
   return result;
  }
-
 }
 
  document.write('<h3>' +good()+ '<h3>' );
